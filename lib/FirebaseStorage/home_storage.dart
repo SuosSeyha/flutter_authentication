19adca81e1dac4205d5da7071e6f920c605f7b0e
Suos Seyha <seyhasuos1202@gmail.com>
2023-06-19 20:38:54 -0700

    Flutter Storage

--- lib/FirebaseStorage/home_storage.dart
+++ lib/FirebaseStorage/home_storage.dart
@@ -3,19 +3,28 @@ import 'package:firebase_storage/firebase_storage.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_native_image/flutter_native_image.dart';
 import 'package:image_picker/image_picker.dart';
+import 'package:liquid_pull_to_refresh/liquid_pull_to_refresh.dart';
 class HomeStorage extends StatefulWidget {
   const HomeStorage({super.key});
-
   @override
   State<HomeStorage> createState() => _HomeStorageState();
 }
 
 class _HomeStorageState extends State<HomeStorage> {
+  
   FirebaseStorage firebaseStorage = FirebaseStorage.instance;
   XFile? xFile;
   File? file;
   String? imageName;
   final ImagePicker picker = ImagePicker();
+  bool isLoading=false;
+  // Refresh image
+  Future <void>refreshImage()async{
+    setState(() async{
+      await downloadImage();
+    });
+    return Future.delayed(const Duration(seconds: 1));
+  }
   // Compress image
   Future<File> compressImage(File file)async{
     File fileCompress = await FlutterNativeImage.compressImage(file.path,quality: 70);
@@ -25,21 +34,53 @@ class _HomeStorageState extends State<HomeStorage> {
   }
   Future<void> uploadImage({required String type})async{
     xFile = await picker.pickImage(source: type=='Camera'? ImageSource.camera: ImageSource.gallery);
-    if(xFile!=null){
-      setState(() async{
+    if(xFile==null) return;
+     
         file = File(xFile!.path);
         imageName=xFile!.name;
+        
         // call method compressimage
         File fileCompress = await compressImage(file!);
         // Upload to firebase storage
+        
         try{
+          setState(() {
+            isLoading=true;
+          });
           await firebaseStorage.ref(imageName).putFile(fileCompress);
+          setState(() {
+            isLoading=false;
+          });
+          ScaffoldMessenger.of(context).showSnackBar(const SnackBar(content: Text('Upload image successful')));
+          // refresh
+          //await downloadImage();
+         // ignore: use_build_context_synchronously
+         
         }on FirebaseException catch(e){
           // ignore: avoid_print
           print(e.message);
         }
+  }
+
+  Future<List> downloadImage()async{
+    List<Map> files=[];
+    final ListResult result = await firebaseStorage.ref().listAll();
+    final List<Reference> allFiles = result.items;
+    await Future.forEach(allFiles, (Reference file) async{
+      final String fileUrl = await file.getDownloadURL();
+      files.add({
+        'url':fileUrl,
+        'path':file.fullPath
       });
-    }
+      debugPrint('url: $fileUrl');
+      debugPrint('path: ${file.fullPath}');
+    });
+    return files;
+  }
+
+  Future <void> deleteImage(String imageName)async{
+    await firebaseStorage.ref(imageName).delete();
+    debugPrint(' Delete image successful.');
   }
   @override
   Widget build(BuildContext context) {
@@ -69,23 +110,52 @@ class _HomeStorageState extends State<HomeStorage> {
         ),
         ],
       ),
-      body: ListView.builder(
-        itemBuilder: (context, index) {
-          return Container(
-            margin: const EdgeInsets.only(
-              top: 10,
-              right: 10,
-              left: 10
-            ),
-            height: height/2 *0.6,
-            width: width,
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(20),
-              color: Colors.red
-            ),
-          );
-        },
-      ),
-    );
+      body: isLoading ? const Center(
+        child: CircularProgressIndicator(),
+      ) : LiquidPullToRefresh(
+        animSpeedFactor: 20,
+        onRefresh: refreshImage,
+        child: FutureBuilder(
+          future: downloadImage(),
+          builder: (context, snapshot) {
+            if(snapshot.connectionState == ConnectionState.waiting){
+              return const Center(
+                child: CircularProgressIndicator(),
+              );
+            }
+            if(snapshot.hasError){
+              return const Center(
+                child: Text('No image'),
+              );
+            }
+            return ListView.builder(
+              itemCount: snapshot.data!.length,
+              itemBuilder: (context, index) {
+                final Map image = snapshot.data![index];
+                return Container(
+                  margin: const EdgeInsets.only(
+                    top: 10,
+                    right: 10,
+                    left: 10
+                  ),
+                  height: height/2 *0.6,
+                  width: width,
+                  decoration: BoxDecoration(
+                    borderRadius: BorderRadius.circular(20),
+                    color: Colors.red,
+                    image: DecorationImage(
+                      fit: BoxFit.fill,
+                      image: NetworkImage(
+                        image['url']
+                      )
+                    )
+                  ),
+                );
+              },
+            );
+          },
+        ),
+      )
+    ); 
   }
 }
